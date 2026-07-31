75b0555efb610c31764758f03d9d755882cc4242
Jota2105 <juanis.albarra@hotmail.com>
2026-07-31 15:17:50 -0500

    feat: agrega modelo inmutable de producto y logica funcional

---
+++ src/main/java/ec/edu/espe/agrosmart/domain/Producto.java
@@ -0,0 +1,44 @@
+package ec.edu.espe.agrosmart.domain;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public final class Producto {
+
+    private final Long id;
+    private final String nombre;
+    private final String categeoria;
+    private final BigDecimal precioUsd;
+    private final List<String> correosNotificacion;
+
+    public Producto(Long id, String nombre, String categeoria, BigDecimal precioUsd, List<String> correosNotificacion){
+        this.id = id;
+        this.nombre = nombre;
+        this.categeoria = categeoria;
+        this.precioUsd = precioUsd;
+        this.correosNotificacion = new ArrayList<>(correosNotificacion);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCategeoria() {
+        return categeoria;
+    }
+
+    public BigDecimal getPrecioUsd() {
+        return precioUsd;
+    }
+
+    public List<String> getCorreosNotificacion() {
+        return Collections.unmodifiableList(new ArrayList<>(correosNotificacion)); // Copia que no se puede modificar
+    }
+}
